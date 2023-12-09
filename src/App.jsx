d11028835a37934fab5f676c3eeca2a362987583
aejmcclelland <aejmcclelland@googlemail.com>
2023-12-09 12:33:20 +0000

    uncommented object properties in app.,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,23 +34,23 @@ import PropertyList from './PropertyList'
 // ]
 
 
-// const properties = [
-//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-// ];
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
 
 
-const data = [
-  { id: 1, item: 'coffee', quantity: 1, completed: true },
-  { id: 2, item: 'milk', quantity: 1, completed: false },
-  { id: 3, item: 'carrots', quantity: 5, completed: false },
-  { id: 4, item: 'eggs', quantity: 12, completed: false },
-  { id: 5, item: 'tins of soup', quantity: 6, completed: false }
-]
+// const data = [
+//   { id: 1, item: 'coffee', quantity: 1, completed: true },
+//   { id: 2, item: 'milk', quantity: 1, completed: false },
+//   { id: 3, item: 'carrots', quantity: 5, completed: false },
+//   { id: 4, item: 'eggs', quantity: 12, completed: false },
+//   { id: 5, item: 'tins of soup', quantity: 6, completed: false }
+// ]
 
 
 function App() {
